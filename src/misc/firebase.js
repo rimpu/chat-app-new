982f77dd3a5441cee76acf534be52925000c9053
aditibo <aditi.bose@citrix.com>
2020-10-26 19:27:13 +0530

    Initialised firebase inside our project

---
+++ src/misc/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app"
+
+const config = {
+  apiKey: "AIzaSyD7mVfkN2eU25BJ-tJJ1A9dIUC_nNZWXM0",
+  authDomain: "chat-web-app-fcc93.firebaseapp.com",
+  databaseURL: "https://chat-web-app-fcc93.firebaseio.com",
+  projectId: "chat-web-app-fcc93",
+  storageBucket: "chat-web-app-fcc93.appspot.com",
+  messagingSenderId: "200275740760",
+  appId: "1:200275740760:web:d7734405fc0cbeda4b622d"
+};
+
+const app = firebase.initializeApp(config);
+
