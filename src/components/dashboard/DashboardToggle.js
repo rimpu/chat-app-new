76ba458817b512ac11180b8f94aa8524f22b5cbf
aditibo <aditi.bose@citrix.com>
2020-10-28 10:00:57 +0530

    Added mediaquery for dashboard toggle

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Button, Icon, Drawer } from 'rsuite'
-import { useModalState } from '../../misc/customHooks'
+import { useModalState, useMediaQuery } from '../../misc/customHooks'
 import Dashboard from '.'
 
 const DashboardToggle = () => {
 
   const {open,close,isOpen} = useModalState();
 
+  const isMobile = useMediaQuery('(max-width : 992px)')
+
   return (
     <>
       <Button block color="blue" onClick={open}>
         <Icon icon="dashboard" />Dashboard
       </Button>
-      <Drawer show={isOpen} onHide={close} placement="left">
+      <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
           <Dashboard/>
       </Drawer>
     </>
